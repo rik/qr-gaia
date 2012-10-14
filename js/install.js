69bccdc5b1125c7bdfaf7fb0f3f4f3a3b93570db
Anthony Ricaud <anthony@ricaud.me>
2012-10-14 09:28:46 -0700

    Add an install button

--- js/install.js
+++ js/install.js
@@ -1,25 +1,30 @@
 'use strict';
 
-(function() {
-var request = navigator.mozApps.getSelf();
-request.onsuccess = function() {
-  if (request.result) {
-    // we're installed
-  } else {
-    var manifestUrl = 'http://localhost:8001/manifest.webapp';
-    request = window.navigator.mozApps.install(manifestUrl);
+window.addEventListener('load', function installLoad(evt) {
+  function install() {
+    var manifestUrl = window.location + '/manifest.webapp';
+    var request = window.navigator.mozApps.install(manifestUrl);
     request.onsuccess = function() {
-      // Save the App object that is returned
       var appRecord = this.result;
       window.alert('Installation successful!');
     };
     request.onerror = function() {
       // Display the error information from the DOMError object
-      window.alert('Install failed, error: ' + this.error.name);
+      log('Install failed, error: ' + this.error.name);
     };
   }
-};
-request.onerror = function() {
-  window.alert('Error checking installation status: ' + this.error.message);
-};
-})();
+
+  var request = navigator.mozApps.getSelf();
+  request.onsuccess = function() {
+    if (request.result) {
+      // we're installed, nothing to do
+    } else {
+      var button = document.getElementById('install-app');
+      button.addEventListener('click', install);
+      button.hidden = false;
+    }
+  };
+  request.onerror = function() {
+    log('Error checking installation status: ' + this.error.message);
+  };
+});
